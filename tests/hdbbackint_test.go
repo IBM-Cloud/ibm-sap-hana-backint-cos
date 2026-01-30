94b615df5766defd614fa46146a0f41e5ca87f1f
Sabine Jaeschke <SAJAESCH@de.ibm.com>
2026-01-30 14:59:17 +0100

    Testing

--- tests/hdbbackint_test.go
+++ tests/hdbbackint_test.go
@@ -1,10 +1,49 @@
 package tests
 
 import (
+	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"testing"
 )
+
+func getTmpDir(t *testing.T) string {
+	tmpDir := t.TempDir()
+	return tmpDir
+}
+
+func createApiKeyFile(t *testing.T, tmpDir string) string {
+	apiKeyFile := filepath.Join(tmpDir, "apikey")
+	if err := os.WriteFile(apiKeyFile, []byte("dummy"), 0644); err != nil {
+		t.Fatalf("cannot create catalog file: %v", err)
+	}
+	return apiKeyFile
+}
+
+func getConfigFile(
+	t *testing.T,
+	tmpDir string,
+	templateFile string,
+) string {
+	apiKeyFile := createApiKeyFile(t, tmpDir)
+	cfgBytes, err := os.ReadFile(templateFile)
+	if err != nil {
+		t.Fatalf("cannot read config template: %v", err)
+	}
+
+	cfgContent := strings.ReplaceAll(
+		string(cfgBytes),
+		"{{APIKEYFILE}}",
+		apiKeyFile,
+	)
+
+	cfgPath := filepath.Join(tmpDir, "hdbbackint.cfg")
+	if err := os.WriteFile(cfgPath, []byte(cfgContent), 0644); err != nil {
+		t.Fatalf("cannot write config: %v", err)
+	}
+	return cfgPath
+}
 
 func getExecutable(t *testing.T) string {
 	return filepath.Join("..", "bin", "hdbbackint")
@@ -23,9 +62,9 @@ func TestVersion(t *testing.T) {
 	}
 }
 
-func TestCheckConfig(t *testing.T) {
-	cfgPath := "../testdata/test.cfg"
-
+func TestCheckConfigSuccess(t *testing.T) {
+	templateFile := "../testdata/test_success.cfg"
+	cfgPath := getConfigFile(t, getTmpDir(t), templateFile)
 	cmd := exec.Command(
 		getExecutable(t),
 		"-p", cfgPath,
@@ -34,6 +73,22 @@ func TestCheckConfig(t *testing.T) {
 
 	output, err := cmd.CombinedOutput()
 	if err != nil {
+		t.Fatalf("hdbbackint -check failed: %v\nOutput: %s", err, output)
+	}
+}
+
+func TestCheckConfigFail(t *testing.T) {
+	templateFile := "../testdata/test_fail.cfg"
+	cfgPath := getConfigFile(t, getTmpDir(t), templateFile)
+
+	cmd := exec.Command(
+		getExecutable(t),
+		"-p", cfgPath,
+		"-check",
+	)
+
+	output, err := cmd.CombinedOutput()
+	if err == nil {
 		t.Fatalf("hdbbackint -check failed: %v\nOutput: %s", err, output)
 	}
 }
