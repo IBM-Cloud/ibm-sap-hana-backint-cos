e4639e55e8d985da5050a3a2e068223f26eb35bf
SabineJaeschke <30996377+SabineJaeschke@users.noreply.github.com>
2026-02-09 16:15:26 +0100

    Adding copyright statements (#41)

--- tests/config_test.go
+++ tests/config_test.go
@@ -1,3 +1,17 @@
+// Copyright 2026 IBM Corp.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License
+
 package tests
 
 import (
@@ -16,18 +30,18 @@ func TestCheckConfig(t *testing.T) {
 
 			if cfgTest.shouldSucceed {
 				if err != nil {
-					t.Fatal("hdbbackint -check should succeed but failed")
+					t.Error("hdbbackint -check should succeed but failed")
 				} else {
 					t.Log("Config check passed")
 				}
 			} else {
 				if err == nil {
-					t.Fatal("hdbbackint -check should fail but succeeded")
+					t.Error("hdbbackint -check should fail but succeeded")
 				} else {
 					if errMsgOk(string(output), cfgTest.msgToCheck) {
 						t.Log("Config check passed")
 					} else {
-						t.Fatalf("Wrong error message(s): \nOutput: %s", output)
+						t.Errorf("Wrong error message(s): \nOutput: %s", output)
 					}
 				}
 			}
--- tests/hdbbackint_test.go
+++ tests/hdbbackint_test.go
@@ -1,3 +1,17 @@
+// Copyright 2026 IBM Corp.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License
+
 package tests
 
 import (
@@ -9,11 +23,11 @@ import (
 func TestVersion(t *testing.T) {
 	output, err := runCommand(t, getExecutablePath(), "-v")
 	if err != nil {
-		t.Fatalf("hdbbackint -v failed: %v\nOutput: %s", err, output)
+		t.Errorf("hdbbackint -v failed: %v\nOutput: %s", err, output)
 	}
 
 	if len(output) == 0 {
-		t.Fatal("expected version output, got empty output")
+		t.Error("expected version output, got empty output")
 	}
 
 	t.Logf("Version output: %s", string(output))
